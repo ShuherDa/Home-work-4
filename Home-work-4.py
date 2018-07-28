1d0e8d2ee3787772e80fe221287a230530e8f33d
Zarubin Alexandr <shuherda@gmail.com>
2018-07-28 14:58:29 +0600

    Исправление ошибок

--- Home-work-4.py
+++ Home-work-4.py
@@ -1,118 +1,93 @@
-class animal:
+class Animal:
     name = ""
     weight = 0
     food = 0
     weight_all = 0
 
+    def __init__(self, name, weight):
+       self.name = name
+       self.weight = weight
+       all_weight[name] = weight
+
     def feed(self, value):
         self.food += value
 
-
-class milk_animals:
+class MilkAnimals:
     milk_liter = 0
     def milk(self, value):
         self.milk_liter += value
 
-class bird:
+class Bird:
     eggs = 0
 
     def collect_eggs(self, egg):
         self.eggs += egg
 
-class cow(milk_animals, animal):
+class Cow(MilkAnimals, Animal):
     voice = "mu mu"
 
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
-
-class goat(milk_animals, animal):
+class Goat(MilkAnimals, Animal):
     voice = "be be"
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
 
-
-class sheep(animal):
+class Sheep(Animal):
     voice = "me me"
     wools = 0
-
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
-
 
     def shear(self, wool):
         self.wools += wool
 
-class goose(bird, animal):
+class Goose(Bird, Animal):
     voice = "ga ga ga"
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
 
-
-class chiken(bird, animal):
+class Chiken(Bird, Animal):
     voice = "ko ko ko"
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
 
-
-class duck(bird, animal):
+class Duck(Bird, Animal):
     voice = "cria cria"
-    def __init__(self, name, weight):
-       self.name = name
-       self.weight = weight
-       all_weight[name] = weight
+
 
 
 # print(cow.mro())
 
 all_weight = {}
 
-cow = cow("Манька", 400)
+cow = Cow("Манька", 400)
 cow.feed(20)
 cow.milk(50)
 
-Sheep1 = sheep("Барашек", 50)
+Sheep1 = Sheep("Барашек", 50)
 Sheep1.shear(10)
 Sheep1.feed(10)
 
-Sheep2 = sheep("Кудрявый", 45)
+Sheep2 = Sheep("Кудрявый", 45)
 Sheep1.shear(15)
 Sheep2.feed(10)
 
-goat1 = goat("Рога", 40)
+goat1 = Goat("Рога", 40)
 goat1.milk(10)
 goat1.feed(8)
 
-goat2 = goat("Копыта", 42)
+goat2 = Goat("Копыта", 42)
 goat2.milk(10)
 goat2.feed(8)
 
-goose1 = goose("Серый", 5)
+goose1 = Goose("Серый", 5)
 goose1.collect_eggs(2)
 goose1.feed(3)
 
-goose2 = goose("Белый", 4)
+goose2 = Goose("Белый", 4)
 goose2.collect_eggs(3)
 goose2.feed(3)
 
-chiken1 = chiken("Кукареку", 3)
+chiken1 = Chiken("Кукареку", 3)
 chiken1.collect_eggs(5)
 chiken1.feed(3)
 
-chiken2 = chiken("Ко-Ко", 2)
+chiken2 = Chiken("Ко-Ко", 2)
 chiken2.collect_eggs(4)
 chiken2.feed(3)
 
-duck = duck("Кряква", 2)
+duck = Duck("Кряква", 2)
 duck.collect_eggs(3)
 duck.feed(3)
 
